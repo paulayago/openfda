ef9c010ca5bb893088af0ea636c9cbfd5f8b4a49
paulayagoesparza <p.yago.2017@alumnos.urjc.es>
2018-05-10 10:17:38 +0200

    Code for the openfda-4 practice

--- openfda-4/practice4.py
+++ openfda-4/practice4.py
@@ -1,33 +1,12 @@
+# -- IP and the port of the server
+IP = "localhost"  # Localhost means "I": your local machine
+PORT = 8000
 import http.server
 import socketserver
 import http.client
 import json
 
 socketserver.TCPServer.allow_reuse_address = True
-
-headers = {'User-Agent': 'http-client'}
-
-conn = http.client.HTTPSConnection("api.fda.gov")
-conn.request("GET", "/drug/label.json?limit=10", None, headers)
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-repos_raw = r1.read().decode("utf-8")
-conn.close()
-
-repos = json.loads(repos_raw)
-drugs = []
-
-for i in range (len (repos['results'])):
-    aspirin = repos['results'][i]
-    if (aspirin['openfda']) == {}:
-        drugs.append("")
-    else:
-        drugs.append(aspirin["openfda"]["generic_name"][0])
-
-# -- IP and the port of the server
-IP = "localhost"  # Localhost means "I": your local machine
-PORT = 8000
-
 
 # HTTPRequestHandler class
 class testHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
@@ -38,26 +17,35 @@ class testHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
         # Send headers
         self.send_header('Content-type', 'text/html')
         self.end_headers()
-        if self.path == "/":
-            # Send message back to client
-            with open("search.html") as f:
+        if self.path == "/" :
+            print("Searching...")
+            with open("search.html", 'r') as f:
                 message = f.read()
-        # Write content as utf-8 data
-        elif self.path == "/new":
-            with open("new.html", "r") as f:
-                new = f.read()
-                message = new
-        else:
-            with open("error.html", "r") as f:
-                error = f.read()
-                message = error
+                # Write content as utf-8 data
+                self.wfile.write(bytes(message, "utf8"))
 
-        self.wfile.write(bytes(message, "utf8"))
-        print("File served!")
+        elif 'search' in self.path: # let´s try to find a drug and a limit entered by user
+            headers = {'User-Agent': 'http-client'}
+            conn = http.client.HTTPSConnection("api.fda.gov")
+            #From the search path we remove "/search" and the "?", then we separate each word by ""
+            search = self.path.strip('/search?').split('""')
+            #Now we assign each variable its correspondent information
+            drug_label = search[0].split('=')[1]
+            limit_drug = search[1].split('=')[1]
+
+            url = "/drug/label.json?search=active_ingredient:"+ drug_label + '&' + 'limit=' + limit_drug
+
+            conn.request("GET", url, None, headers)
+            r1 = conn.getresponse()
+            print(r1.status, r1.reason)
+            repos_raw = r1.read().decode("utf-8")
+
+            conn.close()
+            repos = json.loads(repos_raw)
+            self.wfile.write(bytes(json.dumps(repos), "utf8"))
         return
 
-
-# Handler = http.server.SimpleHTTPRequestHandler
+#Handler = http.server.SimpleHTTPRequestHandler
 Handler = testHTTPRequestHandler
 
 httpd = socketserver.TCPServer((IP, PORT), Handler)
@@ -68,5 +56,4 @@ except KeyboardInterrupt:
     pass
 
 httpd.server_close()
-print("")
 print("Server stopped!")
